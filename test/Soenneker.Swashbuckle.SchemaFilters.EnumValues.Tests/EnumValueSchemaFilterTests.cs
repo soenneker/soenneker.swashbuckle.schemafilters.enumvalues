b1844b1078901db94c184b6b819578f1c16a7e07
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:43 -0500

    TUnit conversion

--- test/Soenneker.Swashbuckle.SchemaFilters.EnumValues.Tests/EnumValueSchemaFilterTests.cs
+++ test/Soenneker.Swashbuckle.SchemaFilters.EnumValues.Tests/EnumValueSchemaFilterTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Swashbuckle.SchemaFilters.EnumValues.Tests;
 
 public sealed class EnumValueSchemaFilterTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
